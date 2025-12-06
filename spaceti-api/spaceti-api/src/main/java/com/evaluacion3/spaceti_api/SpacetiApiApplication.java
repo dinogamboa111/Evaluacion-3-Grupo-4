ff2f790641a17c76ea8adb893e741c262ccc8b85
dinogamboa111 <din.gamboa@duocuc.cl>
2025-12-06 12:28:58 -0300

    spring init

---
+++ spaceti-api/spaceti-api/src/main/java/com/evaluacion3/spaceti_api/SpacetiApiApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion3.spaceti_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacetiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacetiApiApplication.class, args);
+	}
+
+}
---
+++ spaceti-api/spaceti-api/src/test/java/com/evaluacion3/spaceti_api/SpacetiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion3.spaceti_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacetiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
